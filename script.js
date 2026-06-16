99111b2b3ee03f9327ba6a40d24555803238ccab
Oksana Budko <budkoksana@gmail.com>
2026-06-16 18:34:32 +0300

    feat(lesson06/extra): add attempts counter

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ const isNumber = function (num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
 }
 
+
+function resetGame(game) {
+    let option = confirm("Do you want play again?");
+    if (option) {
+        let newGame =  game();
+        newGame();
+    }
+}
+
+
+
 function startGameGuessNumber() {
     const chosenNumber = parseInt(Math.random() * 100);
+    let attemptscounter = 10;
 
     return function getHint() {
+        if (attemptscounter === 0 ) {
+            resetGame(startGameGuessNumber);
+        }
         let userNumber = prompt("Enter a number from 1 to 100:");
         if (userNumber === null) {
             alert("Game is over");
@@ -19,13 +34,16 @@ function startGameGuessNumber() {
                 break;
             case +userNumber === chosenNumber:
                 alert("You win!");
+                resetGame(startGameGuessNumber);
                 break;
             case +userNumber > chosenNumber:
-                alert("Chosen number is less then yours. Try again");
+                attemptscounter--;
+                alert(`Chosen number is less then yours.${attemptscounter} attempts left. Try again`);
                 getHint();
                 break;
             case +userNumber < chosenNumber:
-                alert("Chosen number is greater then yours. Try again");
+                attemptscounter--;
+                alert(`Chosen number is greater then yours.${attemptscounter} attempts left. Try again`);
                 getHint();
                 break;
             default:
